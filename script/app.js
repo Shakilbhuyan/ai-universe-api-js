66371ec61691892ff33bb5780afc87173a455d86
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-03-05 11:41:41 +0600

    modal complete

--- script/app.js
+++ script/app.js
@@ -66,9 +66,9 @@ const displayAiDetail = ai =>{
     console.log(ai);
 const modalBody =document.getElementById('ai-details');
    modalBody.innerHTML = `
-   <div class="container d-flex justify-content-around">
-   <div class ="border border-danger-subtle bg-warning-subtle">
-       <h3>${ai.description}</h3>
+   <div class="d-flex justify-content-around gap-2">
+   <div class ="border border-danger-subtle bg-warning-subtle w-75">
+       <h6>${ai.description}</h6>
        <div class="d-flex justify-content-around gap-2 ">
            <p class="border border-danger-subtle">${ai.pricing[0].price ? ai.pricing[0].price : 'free of cost'} per ${ai.pricing[0].plan}</p>
            <p class="border border-danger-subtle">${ai.pricing[1].price ? ai.pricing[1].price : 'free of cost'} per ${ai.pricing[1].plan}</p>
@@ -76,7 +76,7 @@ const modalBody =document.getElementById('ai-details');
        </div>
        <div class="d-flex justify-content-around">
           <div>
-           <h1>Features</h1>
+           <h4>Features</h4>
            <ul>
                <li>${ai.features[1].feature_name ? ai.features[1].feature_name : 'no features'}</li>
                <li>${ai.features[2].feature_name ? ai.features[2].feature_name : 'no features'}</li>
@@ -84,7 +84,7 @@ const modalBody =document.getElementById('ai-details');
            </ul>
            </div>
           <div>
-           <h1>Integrations</h1>
+           <h4>Integrations</h4>
            <ul>
                <li>${ai.integrations[0] ?ai.integrations[0] : 'no intregations'}</li>
                <li>${ai.integrations[1] ?ai.integrations[1] : 'no intregations'}</li>
@@ -94,10 +94,10 @@ const modalBody =document.getElementById('ai-details');
        </div>
    </div>
    <div>
-       <img class="img-fluid" src="${ai.logo}" alt="">
-       <button></button>
-       <p></p>
-       <p></p>
+       <img class="img-fluid" src="${ai.image_link[0]}" alt="">
+       <button class="bg-danger">Accuracy: ${ai.accuracy.score*100 ? ai.accuracy.score*100 : 'no accuarcy'}%</button>
+       <p>${ai.input_output_examples[0].input ? ai.input_output_examples[0].input : 'no text'}</p>
+       <p>${ai.input_output_examples[0].output ? ai.input_output_examples[0].output : 'no reply'}</p>
    </div>
 </div>
    `;
