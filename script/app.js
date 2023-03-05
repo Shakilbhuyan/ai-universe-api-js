71fbbd40410f5c21c8f47a4854ae92d93f20d9cd
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-03-05 09:53:37 +0600

    spinner added

--- script/app.js
+++ script/app.js
@@ -7,25 +7,51 @@ const loadAiInfo = async()=>{
 }
 const displayAiInfo = ais =>{
   const aiContainer = document.getElementById('ai-container');
-    
-  ais.forEach(ai => {
+//   display only 6 card
+const showAll = document.getElementById('show-all');
+if(ais.length > 6){
+    ais = ais.slice(0,6);
+    showAll.classList.remove('d-none');
+}
+else{
+    showAll.classList.add('d-none')
+}
+    // display all
+  ais.forEach (ai => {
     const aiDiv = document.createElement('div');
     aiDiv.innerHTML = `
     <div class="card">
-    <img src="${ai.image}" class="card-img-top" alt="...">
+    <img class="img-fluid" src="${ai.image}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Features</h5>
           <ol>
-               <li>${ai.features[0] ? ai.features[0] : ''}</li>
-               <li>${ai.features[1] ? ai.features[1] : ''}</li>
-               <li>${ai.features[2] ? ai.features[2] : ''}</li>
-         </ol>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+               <li>${ai.features[0] ? ai.features[0] : 'No features'}</li>
+               <li>${ai.features[1] ? ai.features[1] : 'No features'}</li>
+               <li>${ai.features[2] ? ai.features[2] : 'No features'}</li>
+         </ol><hr>
+      <p class="card-text fs-3 fw-bolder">${ai.name}.</p>
+      <p><i class="fa-regular fa-calendar-days"></i> ${ai.published_in}</p>
+      <div class="mx-auto text-warning"><i class="fa-solid fa-arrow-right"></i></div>
     </div>
   </div>
     `;
     aiContainer.appendChild(aiDiv);
   });
- 
-}
+  toggleSpinner(false)
+};
+
+// loader
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+};
+toggleSpinner(true);
+
+
+
 loadAiInfo();
