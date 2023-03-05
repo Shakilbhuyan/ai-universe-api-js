2e55e521e0f4eec426c9cccdf9802df696544a6b
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-03-05 08:14:36 +0600

    load function added

--- script/app.js
+++ script/app.js
@@ -1,1 +1,6 @@
-console.log('file added');
+const loadAiInfo = async()=>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
